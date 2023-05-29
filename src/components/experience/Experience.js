ac03a4fc6c51d567b8b3a05f629d4a37dfe84bdc
Grace422 <118190954+Grace422@users.noreply.github.com>
2023-05-29 11:21:10 +0100

    experience section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,15 +1,68 @@
 import React from "react";
 import "./experience.css";
-//import ListExperiences from "./components/listExperiences";
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 export const Experience = () => {
   return (
     <section id="experience">
       <h5>What skills i have</h5>
       <h2>My Experience</h2>
-      {/* <div className="container experience__container">
-         <ListExperiences /> 
-      </div> */}
+
+      <div className="container experience__container">
+        <div className="experince__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experince__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" />
+              <div>
+              <h4>HTML</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" />
+             <div>
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+             </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" />
+              <div>
+                <h4>JAVASCRIPT</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experince__backend">
+        <h3>Backend Development</h3>
+          <div className="experince__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" /> 
+              <div>
+                <h4>Mongo DB</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details__icon" />
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,87 @@
 import React from 'react'
+import './portfolio.css'
+import IMG1 from '../../assets/img1.png'
+import IMG2 from '../../assets/img2.png'
+import IMG3 from '../../assets/img3.png'
+import IMG4 from '../../assets/img4.png'
+import IMG5 from '../../assets/img5.png'
+import IMG6 from '../../assets/img7.png'
+
+
 export const Portfolio = () => {
 return (
-<div>
-   Portfolio
-</div>
+<section id="portfolio">
+   <h5>My Recent Work</h5>
+   <h2>Portfolio</h2>
+
+   <div className="container porfolio__container">
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG1} alt="" /> 
+         </div>
+         <h3>Project 1</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG2} alt="" /> 
+         </div>
+         <h3>Project 2</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG3} alt="" /> 
+         </div>
+         <h3>Project 3</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG4} alt="" /> 
+         </div>
+         <h3>Project 4</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG5} alt="" /> 
+         </div>
+         <h3>Project 5</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+
+      <article className="portfolio__item">
+         <div className="portfolio__item-image">
+             <img src={IMG6} alt="" /> 
+         </div>
+         <h3>Project 6</h3>
+        <div className="portfolio__item__cta">
+         <a href='https://github.com' className="btn">Github</a>
+         <a href='https://dribbble.com/shots/20177733-Apres-Brand-Identity-Design' className="btn btn-primary" target="_blank">Live Demo</a>
+        </div>
+      </article>
+  </div>
+</section>
 )
 }
 export default Portfolio;
