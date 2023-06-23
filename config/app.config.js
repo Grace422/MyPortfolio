9a2f282917bf8c828aab32bcbaf8fbb30831ca4e
Grace422 <118190954+Grace422@users.noreply.github.com>
2023-06-23 12:19:24 +0100

    Update app.config.js

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
  require("dotenv").config;
 
  const MONGO_DB_CONFIG = {
-    DB: 'mongodb+srv://kwahamgrace:kwahamgrace@cluster0.bk0obc3.mongodb.net/PortfolioBackend?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://kwahamgrace:kwahamgrace@PortfolioBackend.bk0obc3.mongodb.net/PortfolioBackend?retryWrites=true&w=majority'
  }
 
  module.exports = {
